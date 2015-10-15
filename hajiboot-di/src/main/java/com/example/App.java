99ecd43e5b37daf128379da4c3ae88d2bf1c5767
miki1029 <miki1029g@gmail.com>
2015-10-16 01:40:54 +0900

    CommandLineRunner 형태로 변경(Frontend 삭제)

--- hajiboot-di/src/main/java/com/example/App.java
+++ hajiboot-di/src/main/java/com/example/App.java
@@ -1,9 +1,12 @@
 package com.example;
 
-import com.example.app.Frontend;
+import com.example.app.Argument;
+import com.example.app.ArgumentResolver;
+import com.example.app.Calculator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -12,17 +15,21 @@ import org.springframework.context.annotation.ComponentScan;
 @EnableAutoConfiguration
 //@Import(AppConfig.class) // @Configuration이 붙은 클래스
 @ComponentScan // 패키지 내부에 있는 모든 클래스를 검색(패키지 변경하려면 basePackages 속성 지정)(@Component)
-public class App {
+public class App implements CommandLineRunner {
+    @Autowired
+    ArgumentResolver argumentResolver;
+    @Autowired
+    Calculator calculator;
+
+    @Override
+    public void run(String... strings) throws Exception {
+        System.out.print("Enter 2 numbers like 'a b' : ");
+        Argument argument = argumentResolver.resolve(System.in);
+        int result = calculator.calc(argument.getA(), argument.getB());
+        System.out.println("result = " + result);
+    }
+
     public static void main(String[] args) {
-        /*
-         * @param App.class @EnableAutoConfiguration이 붙은 클래스
-         * @return ApplicationContext DI Container
-         * ConfigurableApplicationContext : close()로 닫을 수 있는 타입
-         * try-with-resources 구문
-         */
-        try(ConfigurableApplicationContext context = SpringApplication.run(App.class, args)) {
-            Frontend frontend = context.getBean(Frontend.class);
-            frontend.run();
-        }
+        SpringApplication.run(App.class, args);
     }
 }
